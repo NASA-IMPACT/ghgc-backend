bfd4adceb89217bfa8ab86c915b9e7368f6d8f06
amarouane-ABDELHAK <am0089@uah.edu>
2023-06-26 09:28:09 -0500

    Specify the output of CF

--- stac_api/infrastructure/construct.py
+++ stac_api/infrastructure/construct.py
@@ -93,4 +93,4 @@ class StacApiLambdaConstruct(Construct):
             default_domain_mapping=domain_mapping,
         )
 
-        CfnOutput(self, "stac-api", value=stac_api.url)
+        CfnOutput(self, "stac-api", output_key="STAC_API_URL", value=stac_api.url)
